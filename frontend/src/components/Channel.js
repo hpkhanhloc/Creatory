064e3b7c3bc78b63415d84c765e408ff67c24a90
Loc <hpkhanhloc@gmail.com>
2019-12-21 14:16:04 +0700

    Update Channel.js
    editted subscribers_gained display

--- frontend/src/components/Channel.js
+++ frontend/src/components/Channel.js
@@ -9,7 +9,7 @@ const Channel = ({channel}) => {
             <td>{channel.video.measurement_date}</td>
             <td>{channel.video.subscribers_count}</td>
             <td>{channel.video.comments}</td>
-            <td>{channel.video.subscribersgained}</td>
+            <td>{channel.video.subscribers_gained}</td>
             <td>{channel.video.subscriberslost}</td>
             <td>{channel.video.unsub_dislikes}</td>
             <td>{channel.video.unsub_likes}</td>
